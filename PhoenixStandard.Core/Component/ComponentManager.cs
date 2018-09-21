fb328dab145483cbb398a4f07599fa56eea07e59
Coderrob <Rob@Coderrob.com>
2018-09-20 20:33:55 -0500

    Quick stab at component manager.

--- PhoenixStandard.Core/Component/ComponentManager.cs
+++ PhoenixStandard.Core/Component/ComponentManager.cs
@@ -1,26 +1,35 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PhoenixStandard.Core.Component
 {
     public class ComponentManager : IComponentManager
     {
+        private readonly ConcurrentDictionary<long, IComponent> _entities;
+        private readonly ConcurrentBag<IComponent> _components;
         private readonly IComponentFactory<IComponent> _factory;
 
         public ComponentManager(IComponentFactory<IComponent> factory)
         {
             _factory = factory;
+            _entities = new ConcurrentDictionary<long, IComponent>();
+            _components = new ConcurrentBag<IComponent>();            
         }
 
         public bool AddComponentToEntity(long entityId, IComponent component)
         {
-            throw new NotImplementedException();
+            _components.Add(component);
+            return _entities.TryAdd(entityId, component);
         }
 
         public bool EntityHasComponents(long entityId, IEnumerable<Type> componentTypes)
         {
-            throw new NotImplementedException();
+            if (componentTypes == null) return false;
+            return _entities.ContainsKey(entityId) &&
+                   _entities.TryGetValue(entityId, out var component) &&
+                   componentTypes.Contains(component.GetType());
         }
 
         public ConcurrentDictionary<long, T> RegisterComponentType<T>() where T : IComponent
