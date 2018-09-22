7aae5ce9970417d55f332eacf0c52e9496b6ef6e
Jonathan Mast <Jonathan.Mast@Microsoft.com>
2018-09-21 23:08:19 -0400

    ComponentManager is no longer a thing

--- PhoenixStandard.Core/Component/IComponentManager.cs
+++
@@ -1,8 +0,0 @@
-namespace PhoenixStandard.Core.Component
-{
-    public interface IComponentManager
-    {
-        void RegisterComponentType<T>() where T:IComponent, new();
-        void UnregisterComponentType<T>() where T: IComponent, new();
-    }
-}
